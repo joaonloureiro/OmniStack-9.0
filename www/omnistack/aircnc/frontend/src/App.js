9afe048a6f6df1d02dad4f6495c683ab0f0bb60d
João Loureiro <joaohnl@gmail.com>
2019-10-14 08:42:43 -0300

    Finalizado content do index

--- www/omnistack/aircnc/frontend/src/App.js
+++ www/omnistack/aircnc/frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
     <div className="App">
       <div className="container">
+        <img src={logo} alt="Logo AirCnC"/>
         <div className="content-box">
-          
+          <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa</p>
+          <form>
+
+          </form>
         </div>
       </div>
     
