73e8154c7680e8b5a63cb7a0b2ab0f254141d622
João Loureiro <joaohnl@gmail.com>
2019-10-21 11:23:17 -0300

    Implementado URL local

--- www/omnistack/aircnc/frontend/src/App.js
+++ www/omnistack/aircnc/frontend/src/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <img src={logo} alt="Logo AirCnC"/>
-        <div className="content-box">
-          <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa</p>
-          <form>
-            <label htmlFor="email">E-MAIL *</label>
-            <input
-              type="email"
-              name="email" 
-              id="email" 
-              placeholder="Seu melhor email"/>
+  const [email, setEmail] = useState('');
 
-          </form>
-        </div>
-      </div>
+  function handleSubmit(event) {
+    event.preventDefault();
     
+
+  }
+
+  return (
+    <div className="container">
+      <img src={logo} alt="Logo AirCnC"/>
+      <div className="content-box">
+        <p>Ofereça <strong>spots</strong> para programadores e
+        <br/>encontre <strong>talentos</strong> para a sua empresa</p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="email"
+            name="email" 
+            id="email" 
+            placeholder="Seu melhor email"
+            value={email}
+            onChange={event => setEmail(event.target.email)}
+            />
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
